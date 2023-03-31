4d1a359405371ad32e325a6b1afc99eba963cb57
alifisnaini <alifisnaini45@gmail.com>
2023-03-31 09:57:56 +0800

    customer

---
+++ app/Filament/Resources/CustomersResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CustomersResource\Pages;
+use App\Filament\Resources\CustomersResource\RelationManagers;
+use App\Models\Customers;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CustomersResource extends Resource
+{
+    protected static ?string $model = Customers::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+        ->schema([
+            Forms\Components\TextInput::make('nama'),
+            Forms\Components\DatePicker::make('tanggal_lahir')
+            ->minDate(now()->subYears(150))
+            ->maxDate(now()),
+            Forms\Components\Toggle::make('status')
+            ->onColor('success')
+            ->offColor('danger'),
+            Forms\Components\Radio::make('jenis_kelamin')
+            ->options([
+                'L' => 'Laki-laki',
+                'P' => 'perempuan',
+            ]),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nama'),
+                Tables\Columns\TextColumn::make('tanggal_lahir'),
+                Tables\Columns\TextColumn::make('status'),
+                Tables\Columns\TextColumn::make('jenis_kelamin'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCustomers::route('/'),
+            'create' => Pages\CreateCustomers::route('/create'),
+            'edit' => Pages\EditCustomers::route('/{record}/edit'),
+        ];
+    }    
+}
---
+++ database/migrations/2023_03_30_062257_customers.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function($table){
+            $table->id();
+            $table->string('nama')->nullable();
+            $table->text('tanggal_lahir')->nullable();
+            $table->boolean('status')->default(0);
+            $table->string('jenis_kelamin')->default(0);
+            $table->timestamps();
+
+        });
+    }
+ 
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
